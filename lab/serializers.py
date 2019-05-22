086e1c4fd87ed20fe134aee47548d421334c8bb1
何博 <nku_hb@outlook.com>
2019-05-22 10:39:22 +0800

    Delete useless note. #40

--- lab/serializers.py
+++ lab/serializers.py
@@ -3,8 +3,6 @@ from .models import Lab
 
 class LabSerializers1(serializers.ModelSerializer):
     lab_id = serializers.SerializerMethodField()
-    # name = serializers.CharField()
-    # id = serializers.CharField()
 
     class Meta:
         model = Lab
@@ -13,9 +11,3 @@ class LabSerializers1(serializers.ModelSerializer):
     def get_lab_id(self, obj):
         return obj['id']
 
-# class DemoSerializers2(serializers.ModelSerializer):
-
-#     class Meta:
-#         model = Lab
-#         fields = ('content', 'is_public')
-#         # or  fields = '__all__'
