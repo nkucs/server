2d9373a27a7feba2abd0be8030206134357ad8ce
hanlinghao <417564526@qq.com>
2019-05-23 00:24:48 +0800

    Add new serializers for Lab and CourseResource. Ref #41

--- lab/serializers.py
+++ lab/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Lab
+from course.models import CourseResource
 
 class LabSerializers1(serializers.ModelSerializer):
     lab_id = serializers.SerializerMethodField()
@@ -11,3 +12,25 @@ class LabSerializers1(serializers.ModelSerializer):
     def get_lab_id(self, obj):
         return obj['id']
 
+class GetLabFileSerializer(serializers.ModelSerializer):
+    """
+    Serialize 'resources' attribute of a Lab object.
+    API: get-lab
+    """
+    class Meta:
+        model = CourseResource
+        fields = ('id', 'name')
+
+class GetLabSerializer(serializers.ModelSerializer):
+    """
+    Serialize a Lab object.
+    API: get-lab
+    """
+    files = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Lab
+        fields = ('name', 'description', 'start_time', 'end_time', 'report_required', 'files')
+
+    def get_files(self, obj):
+        return GetLabFileSerializer(obj.resources, many=True).data
