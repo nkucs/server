29fdb0940df3fc525ac41b49681a447933617732
何博 <nku_hb@outlook.com>
2019-05-23 16:59:05 +0800

    Add api that can delete a laboratory lesson. (#39)

--- lab/urls/admin.py
+++ lab/urls/admin.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
 from django.urls import path
-from ..views.admin import GetLabsAPI, GetSubmissionFileAPI, GetLabAPI
+from ..views.admin import GetLabsAPI, GetSubmissionFileAPI, GetLabAPI, DeleteLabAPI
 
 urlpatterns = [
     url(r"get-my-labs/?$", GetLabsAPI.as_view(), name="get-my-labs"),
+    path('delete-lab/', DeleteLabAPI.as_view(), name='delete-lab'),
     path('get-submission-file/', GetSubmissionFileAPI.as_view(), name='get-submission-file'),
     path('get-lab/', GetLabAPI.as_view(), name='get-lab'),
 ]
