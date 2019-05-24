63c5d467746fae56073e71fe4e065c32205a48c8
Loein <wx_e11e0e258b834cb78ce9d63e0fd571ff@git.code.tencent.com>
2019-05-24 17:24:32 +0800

    feat: add getLectureAPI (#25)

---
+++ lecture/serializers.py
@@ -0,0 +1,39 @@
+from rest_framework import serializers
+from .models import Lecture
+from course.models import CourseResource
+
+
+class LectureSerializers(serializers.ModelSerializer):
+    lecture_id = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Lecture
+        fields = ('lecture_id', 'name')
+
+    def get_lecture_id(self, obj):
+        return obj['id']
+
+class GetLectureFileSerializer(serializers.ModelSerializer):
+    """
+    Serialize 'resources' attribute of a Lecture object.
+    API: get-lecture
+    """
+
+    class Meta:
+        model = CourseResource
+        fields = ('id', 'name')
+
+class GetLectureSerializer(serializers.ModelSerializer):
+    """
+    Serialize a Lecture object.
+    API: get-lecture
+    """
+
+    files = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Lecture
+        fields = ('name', 'description', 'files')
+
+    def get_files(self, obj):
+        return GetLectureFileSerializer(obj.resources, many=True).data
