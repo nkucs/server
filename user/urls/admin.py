42b387602a0ad2b742d2c7222ab938f65ee389d1
syc <syc_nku@163.com>
2019-05-26 05:58:38 +0800

    feat: add role list api (#68)

--- user/urls/admin.py
+++ user/urls/admin.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
-from user.views.admin import GetRoleAPI
 from user.views.admin import CreateRoleAPI
+from user.views.admin import GetRoleAPI, GetRoleListAPI
 
 urlpatterns = [
     url(r'create_role/?$', CreateRoleAPI.as_view(), name="create_role"),
     url(r'role_detail?$', GetRoleAPI.as_view(), name="get-role"),
+    url(r'role_list?$', GetRoleListAPI.as_view(), name="get-role-list"),
 ]
