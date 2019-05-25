240d4c04dec76395e95e50a4748ad8c2d568b027
RexHac <lizhiqiangsss@outlook.com>
2019-05-25 19:59:20 +0800

    feat: add get lecture by name API (#60)

--- lecture/urls/admin.py
+++ lecture/urls/admin.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
 from lecture.views.admin import CreateLectureAPI
 from lecture.views.admin import GetMyLecturesAPI
+from lecture.views.admin import GetLectureByNameAPI
 urlpatterns = [
     url(r'create-lectures/?$', CreateLectureAPI.as_view(), name="create-lectures"),
     url(r'get-my-lectures/?$', GetMyLecturesAPI.as_view(), name="get-my-lectures"),
+    url(r'get-lecture-by-name/?$', GetLectureByNameAPI.as_view(), name="get-lecture-by-name"),
 ]
--- lecture/views/admin.py
+++ lecture/views/admin.py
@@ -64,3 +64,29 @@ class GetLectureAPI(APIView):
         except Exception as e:
             # not found
             return self.error(msg=str(e), err=e.args)
+
+class GetLectureByNameAPI(APIView):
+    response_class = JSONResponse
+    def get(self, request):
+        # initialize the response object
+        response_object = dict()
+        # get information from frontend
+        try:
+            page = int(request.GET.get('page'))
+            course_id = int(request.GET.get('course_id'))
+            page_length = int(request.GET.get('page_length'))
+            name = request.GET.get('name')
+        except Exception as exception:
+            return self.error(err=exception.args, msg="course_id:%s, page:%s\n"%(request.GET.get('course_id'), request.GET.get('page')))
+        try:
+            # query from database filter the specific data
+            query_set = Lecture.objects.filter( course_id=course_id, name__icontains=name)
+            lectures_amount = query_set.count()
+            lectures_list = query_set[(page - 1) * page_length : page * page_length].values('id', 'name')
+            response_object['total_counts'] = lectures_amount
+            response_object['lectures'] = LectureSerializers(lectures_list, many=True).data
+            return self.success(response_object)
+        except Exception as exception:
+            return self.error(err=exception.args, msg=str(exception))
+
+
