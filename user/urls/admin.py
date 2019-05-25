909190af28f18c9fac34481b155b167169baaecd
Shuang_28 <734450392@qq.com>
2019-05-26 04:26:48 +0800

    feat: add create role (#58)

--- user/urls/admin.py
+++ user/urls/admin.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
 from user.views.admin import GetRoleAPI
+from user.views.admin import CreateRoleAPI
 
 urlpatterns = [
+    url(r'create_role/?$', CreateRoleAPI.as_view(), name="create_role"),
     url(r'role_detail?$', GetRoleAPI.as_view(), name="get-role"),
 ]
