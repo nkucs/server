d94c1b38738d3ecebbbdc7f606977672c374ca97
yuhao <1660754769@qq.com>
2019-05-26 23:57:27 +0800

    feat:add create_problem_api(#33)

--- problem/urls/admin.py
+++ problem/urls/admin.py
@@ -1,4 +1,8 @@
 from django.conf.urls import url
-
+from problem.views.admin import CreateProblemAPI,DeleteProblemAPI,EditProblemAPI,AddTagAPI
 urlpatterns = [
+    url(r"create-problem/?$", CreateProblemAPI.as_view(), name="create-problem"),
+    url(r"edit-problem/?$", EditProblemAPI.as_view(), name="edit-problem"),
+    url(r"delete-problem/?$", DeleteProblemAPI.as_view(), name="delete-problem"),
+    url(r"add-tag/?$",AddTagAPI.as_view(),name="add-tag"),
 ]
--- problem/views/admin.py
+++ problem/views/admin.py
@@ -1,1 +1,50 @@
-from utils.api import APIView
+from utils.api import APIView,JSONResponse
+from problem.models import Problem,Tag,Case
+from user.models import Teacher
+from django.db import models
+
+class CreateProblemAPI(APIView):
+    # response to post request
+    def post(self,request):
+        response_object = dict()
+        # get information from frontend
+        try:
+            problem_name = request.POST.get('problem_name')
+            description = request.POST.get('description')
+            created_teacher_id = request.POST.get('created_teacher_id')
+            runtime_limit = request.POST.get('runtime_limit')
+            memory_limit = request.POST.get('memory_limit')
+            problem_tags = request.POST.get('tags')
+            cases = request.POST.get('cases')
+        except Exception as exception:
+             return self.error(err=exception.args, msg="problem_name:%s, description:%s, created_teacher_id:%s,runtime_limit:%s,memory_limit:%s\n"%(request.POST.get('problem_name'), request.POST.get('description'), request.POST.get('created_teacher_id'), request.POST.get('runtime_limit'), request.POST.get('memory_limit')))
+        # insert new problem and cases to database
+        try:
+            # find teacher from Teacher table
+            teacher = Teacher.objects.get(teacher_number=created_teacher_id)
+            # add problem to  problem table
+            problem = Problem.objects.create(problem_name=problem_name,description=description,teacher=teacher,runtime_limit=runtime_limit,memory_limit=memory_limit)
+            response_object['problem_id'] = problem.id
+
+            # add problem_tags
+            for problem_tag in problem_tags:
+                tag = Tag.objects.get(id=int(problem_tag['tag_id']))
+                problem.tags.add(tag)  # add tag to problem
+
+            # add cases to case table 
+            for case in cases:
+                case_input = case["input"]
+                case_output = case["output"]
+                case_type = case["type"] # zero for test, one for example
+                weight = case["weight"]
+                case_tags = case["tags"]
+                # create case in Case  table
+                case = Case.objects.create(input=case_input,output=case_output,type=case_type,problem=problem,weight=weight)
+                # add case_tags
+                for case_tag in case_tags:
+                    tag = Tag.objects.get(id=int(case_tag['tag_id']))
+                    case.tags.add(tag)  # add tag to case
+
+            return self.success(response_object)
+        except Exception as exception:
+            return self.error(err=exception, msg=str(exception))
