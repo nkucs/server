bc8abaede46aa1d4f6d4f082d1c813a4285d1b80
yanqingdi <wx_d46b7b2598ab49f28501365f96e41976@git.code.tencent.com>
2019-05-26 16:01:02 +0800

    feat: get exam student(#83)

--- exam/urls/admin.py
+++ exam/urls/admin.py
@@ -1,4 +1,8 @@
 from django.conf.urls import url
 
+from exam.views.admin import GetExamStudentAPI
+
 urlpatterns = [
+    url(r"get-examstudent/?$", GetExamStudentAPI.as_view(), name="get-examstudent"),
+   
 ]
