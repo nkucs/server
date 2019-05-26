91cd7bf722cb46b40d908bef746e73b5617cfee9
Zhong Zhenyu <nczzy1997@gmail.com>
2019-05-26 16:04:12 +0800

    fix: fix APIView reference errors (#85)

--- demo/views/oj.py
+++ demo/views/oj.py
@@ -1,8 +1,14 @@
+from rest_framework.permissions import IsAuthenticated
+
 from utils.api import APIView
 from ..serializers import DemoSerializers1
 from ..models import Demo
 
+
 class DemoAPI(APIView):
+    # Add the following line for authentication purposes.
+    # permission_classes = (IsAuthenticated,)
+
     def get(self, request):
         demo_id = request.GET.get('demo_id')
         demo = Demo.objects.get(id=demo_id)
