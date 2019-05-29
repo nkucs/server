526448aa43574697884daf0904244683ef7af10c
LGh1gh <jokerliveswzy@gmail.com>
2019-05-29 09:14:31 -0700

    refactor: optimize get-all/my-course apis.

--- course/urls/admin.py
+++ course/urls/admin.py
@@ -1,4 +1,9 @@
 from django.conf.urls import url
 
+from course.views.admin import GetAllCourseAPI
+from course.views.admin import GetMyCourseAPI
+
 urlpatterns = [
+    url(r"get-all-course/?$", GetAllCourseAPI.as_view(), name="get-all-course"),
+    url(r"get-my-course/?$", GetMyCourseAPI.as_view(), name="get-my-course")
 ]
