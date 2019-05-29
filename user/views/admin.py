3bdf61ef7f4903644336cb70c9a143102ee42ad2
Zhong Zhenyu <nczzy1997@gmail.com>
2019-05-29 14:28:20 +0800

    feat: add role teacher list api paginator (#109)

--- user/views/admin.py
+++ user/views/admin.py
@@ -9,6 +9,7 @@ from ..models import Role, Permission, User
 
 class GetRoleAPI(APIView):
     response_class = JSONResponse
+
     # get方法，参数用params放在url后面
 
     def get(self, request):
@@ -58,7 +59,7 @@ class CreateRoleAPI(APIView):
             name = request.data.get('name')
             description = request.data.get('description')
             permission = request.data.get('permission')
-            if(name is None or description is None or permission is None):
+            if (name is None or description is None or permission is None):
                 raise Exception()
         except Exception as exception:
             msg = "name:%s, description:%s\n" % (
@@ -90,14 +91,14 @@ class GetRoleListAPI(APIView):
             items = int(request.data.get('items_per_page'))
             page = int(request.data.get('page'))
             searchDict = request.data.get("search_request")
-            if(searchDict['role_id'] != ""):
+            if (searchDict['role_id'] != ""):
                 id = int(searchDict['role_id'])
             name = searchDict["role_name"]
             description = searchDict["role_description"]
         except Exception as exception:
             return self.error(err=[400])
         try:
-            if(searchDict['role_id'] != ""):
+            if (searchDict['role_id'] != ""):
                 roles = Role.objects.filter(
                     description__contains=description,
                     group_id=id)
@@ -105,7 +106,7 @@ class GetRoleListAPI(APIView):
                 roles = Role.objects.filter(description__contains=description)
             ans = []
             for role in roles:
-                if(name in role.group.name):
+                if (name in role.group.name):
                     ans.append(role)
             roleList = []
             pages = math.ceil(ans.__len__() / items)
@@ -116,7 +117,7 @@ class GetRoleListAPI(APIView):
                 roleDict['description'] = item.description
                 roleDict['role_number'] = item.group.user_set.count()
                 roleList.append(roleDict)
-            if(roleList.__len__() <= items * (page - 1)):
+            if (roleList.__len__() <= items * (page - 1)):
                 roleList = []
             else:
                 start = items * (page - 1)
@@ -139,7 +140,7 @@ class ModifyRoleAPI(APIView):
             name = request.data.get('name')
             description = request.data.get('description')
             permission = request.data.get('permission')
-            if(name is None or description is None or permission is None):
+            if (name is None or description is None or permission is None):
                 raise Exception()
         except Exception as exception:
             msg = "name:%s, description:%s\n" % (
@@ -163,21 +164,52 @@ class ModifyRoleAPI(APIView):
             return self.error(err=exception.args, msg=response_object)
 
 
+class Paginator:
+
+    def __init__(self, obj_set, serializer, page, page_size):
+        self.__obj_set = obj_set
+        self.__serializer = serializer
+        self.__page = page
+        self.__page_size = page_size
+
+    def get_response(self):
+        objects_count = self.__obj_set.count()
+        pages_count = math.ceil(objects_count / self.__page_size)
+
+        if self.__page > pages_count:
+            self.__page = pages_count
+        if self.__page < 1:
+            self.__page = 1
+        offset = (self.__page - 1) * self.__page_size
+        objs = self.__obj_set.all()[
+                offset:offset + self.__page_size if offset + self.__page_size <= objects_count else objects_count]
+
+        response = {
+            'page': self.__page,
+            'count': objects_count,
+            'total_pages': pages_count,
+            'contents': []
+        }
+        for obj in objs:
+            response['contents'].append(self.__serializer(obj).data)
+
+        return response
+
+
 class GetRoleTeacherListAPI(APIView):
 
     def get(self, request):
         role_id = int(request.GET.get('id', 0))
+        page = int(request.GET.get('page', 1))
+        page_size = int(request.GET.get('page_size', 10))
 
         try:
             group = Group.objects.get(id=role_id)
-        except Model.DoesNotExist:
+        except Group.DoesNotExist:
             return self.error(err=404, msg='Role does not exist.')
 
-        users = group.user_set.all()
-        response = []
-        for user in users:
-            response.append(TeacherSerializer(user).data)
-        return self.success(data=response)
+        paginator = Paginator(group.user_set, TeacherSerializer, page, page_size)
+        return self.success(data=paginator.get_response())
 
 
 class GetRoleAddTeacherListAPI(APIView):
@@ -186,5 +218,3 @@ class GetRoleAddTeacherListAPI(APIView):
 
 class RoleTeacherAPI(APIView):
     pass
-
-
