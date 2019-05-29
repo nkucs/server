197f2e25b7dacfc693ef1da790f9c5cec5e41cbc
HuiBin Zhang <3478038112@qq.com>
2019-05-29 22:42:52 +0800

    更新学生端API接口

--- submission/views/student.py
+++ submission/views/student.py
@@ -1,1 +1,28 @@
 from utils.api import APIView
+from rest_framework import status
+from ..serializers import ProblemSubmissionSerializers1
+from django.http import HttpResponse, JsonResponse
+from ..models import ProblemSubmission
+
+class GetALLSubmissionAPI(APIView):
+    def get(self, request):
+        submission_student_id = int(request.GET.get('submission_student_id'))
+        # query from database for submissions of student
+        try:
+            UserPersonalSubmission = ProblemSubmission.objects.get(id_student=submission_student_id)
+        except UserPersonalSubmission.DoesNotExist:
+            return HttpResponse(status=404)
+        serializer = ProblemSubmissionSerializers1(UserPersonalSubmission)
+        return JsonResponse(serializer.data,status=status.HTTP_200_OK)
+
+class GetUserSubmissionAPI(APIView):
+    def get(self, request):
+        submission_student_id = int(request.GET.get('submission_student_id'))
+        submission_problem_id = int(request.GET.get('submission_problem_id'))
+        # query from database for submissions of student and problem 
+        try:
+            UserPersonalSubmission = ProblemSubmission.objects.filter(id_student=submission_student_id, id_problem=submission_problem_id)
+        except ProblemSubmission.DoesNotExist:
+            return HttpResponse(status=404)
+        serializer = ProblemSubmissionSerializers1(ProblemSubmission)
+        return JsonResponse(serializer.data,status=status.HTTP_200_OK)
--- user/views/student.py
+++ user/views/student.py
@@ -1,1 +1,16 @@
-from utils.api import APIView
+from utils.api import APIView, JSONResponse
+from rest_framework import status
+from ..serializers import ProblemSubmissionSerializers1
+from django.http import HttpResponse, JsonResponse
+from ..models import Role, Permission, Student
+
+def GetStudentAPI():
+    def get(self, request):
+        # get information from frontend
+        id_student = int(request.GET.get('id_student'))
+        try:
+            Student = Student.objects.get(id=id_student) 
+        except Student.DoesNotExist:
+            return HttpResponse(status=404)
+        serializer = ProblemSubmissionSerializers1(Student)
+        return JsonResponse(serializer.data,status=status.HTTP_200_OK)
