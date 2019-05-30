55ce6a2b8af17a54065a001256c533329c7b4b2d
XINGYUN XUE <NKCScat@gmail.com>
2019-05-30 17:10:15 +0800

    Update serializers.py
    style: no new line in end of file (#37)

--- problem/serializers.py
+++ problem/serializers.py
@@ -9,3 +9,4 @@ class GetProblemSerializer(serializers.ModelSerializer):
         model = Problem
         fields = ('code', 'name', 'description', 'runtime_limit',
          'memory_limit', 'created_at','modified_at','teacher','tags')
+        
