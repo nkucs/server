f565bfc712f55f4c52d6367c577932dc166efdb5
HuiBin Zhang <3478038112@qq.com>
2019-05-31 20:10:58 +0800

    更新学生登陆API

--- user/views/student.py
+++ user/views/student.py
@@ -4,13 +4,27 @@ from ..serializers import ProblemSubmissionSerializers1
 from django.http import HttpResponse, JsonResponse
 from ..models import Role, Permission, Student
 
-def GetStudentAPI():
+def GetStudentAPI(APIView):
     def get(self, request):
         # get information from frontend
         id_student = int(request.GET.get('id_student'))
         try:
-            Student = Student.objects.get(id=id_student) 
-        except Student.DoesNotExist:
+            NowStudent = Student.objects.get(id=id_student) 
+        except NowStudent.DoesNotExist:
             return HttpResponse(status=404)
-        serializer = ProblemSubmissionSerializers1(Student)
+        serializer = ProblemSubmissionSerializers1(NowStudent)
         return JsonResponse(serializer.data,status=status.HTTP_200_OK)
+
+def LoginStudentAPI(APIView):
+    # Login API
+    def post(self, request):
+        studentName = request.GET.get('name')
+        studentPasswordHash = request.GET.get('password_hash')
+        studentUser = Student.objects.get(name=studentName)
+        if studentUser:
+            if studentUser.password_hash == studentPasswordHash:
+                return HttpResponse(status=0)
+            else:
+                return HttpResponse(status=1)
+        else:
+            return HttpResponse(status=-1)
