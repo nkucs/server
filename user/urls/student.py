23d367e53650ec93cc4eccd21c9516c2c253d52c
qianjinghao <332090529@qq.com>
2019-06-01 19:30:26 +0800

    fix:fix conflict(#146#147)

--- user/urls/student.py
+++ user/urls/student.py
@@ -1,4 +1,8 @@
 from django.conf.urls import url
+from user.views.student import GetStudentListAPI,DeleteStudentAPI
 
 urlpatterns = [
+    url(r'student_list?$', GetStudentListAPI.as_view(), name="get_student_list"),
+    url(r'student_delete?$', DeleteStudentAPI.as_view(), name="delete_student")
+    
 ]
