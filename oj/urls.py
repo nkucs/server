0223583b56494a608eb2705e50c17c8a90d73cfe
caoyuanpu <18222611877@163.com>
2019-06-01 21:38:23 +0800

    课程内题目综合分析提交次数折线图后端实现 蹇坤霖 #125

--- oj/urls.py
+++ oj/urls.py
@@ -32,5 +32,10 @@ urlpatterns = [
     url(r"^teacher/submission/stat/", include("submission.urls.statistics")),
     url(r"^teacher/student/stat/", include("user.urls.statistics")),
     url(r"^api/student/", include("lab.urls.student")),
-    url(r"^teacher/submission/statistics", include("submission.urls.statistics")),
+    url(r"^auth/", include("user.urls.auth")),
+    url(r"^swagger$", schema_view),
+    url(r"^teacher/problem/", include("problem.urls.admin")),
+    url(r"^administrator/student/", include("user.urls.student")),
+    url(r"^teacher/submission/statistics",
+        include("submission.urls.statistics")),
 ]
