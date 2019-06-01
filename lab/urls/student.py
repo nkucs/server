5804cb3dde8c9c73a3272d06455a24ef88f6961a
DingPeng <dp1612841@gmail.com>
2019-06-01 12:52:56 +0800

    修改上传文件的接口地址

--- lab/urls/student.py
+++ lab/urls/student.py
@@ -4,5 +4,5 @@ from ..views.student import LabAPI, LabDetailAPI, PostAttachmentAPI
 urlpatterns = [
     url(r"^lab_course_list/?$", LabAPI.as_view(), name="lab_list_api"),
     url(r"^lab_course_detail/?$", LabDetailAPI.as_view(), name="lab_detail_api"),
-    url(r"^post-attachment/?$", PostAttachmentAPI.as_view(), name='post-attachment')
+    url(r"^lab_attachment_hand_in/?$", PostAttachmentAPI.as_view(), name='post-attachment')
 ]
