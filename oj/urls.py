29d55a53f10ed90056268c897e20f9645d566bc0
jkl <jiandaobao19@qq.com>
2019-06-02 00:06:10 +0800

    添加测例标签后端api.蹇坤霖 #125

--- oj/urls.py
+++ oj/urls.py
@@ -36,6 +36,6 @@ urlpatterns = [
     url(r"^swagger$", schema_view),
     url(r"^teacher/problem/", include("problem.urls.admin")),
     url(r"^administrator/student/", include("user.urls.student")),
-    url(r"^teacher/submission/statistics",
+    url(r"^teacher/submission/statistics/",
         include("submission.urls.statistics")),
 ]
