38ca0061a15422dd04f166f990660dfd7c3b66cb
yanqingdi <2597892733@qq.com>
2019-06-02 22:41:17 +0800

    feat: finish add student page all api(#89)

--- exam/urls/admin.py
+++ exam/urls/admin.py
@@ -1,23 +1,25 @@
 from django.conf.urls import url
 
 from exam.views.admin import GetExamStudentAPI
-from exam.views.admin import GetIdStudentAPI
-from exam.views.admin import GetNameStudentAPI
+# from exam.views.admin import GetIdStudentAPI
+# from exam.views.admin import GetNameStudentAPI
 from exam.views.admin import AddStudentAPI
 from exam.views.admin import DeleteStudentAPI
 from exam.views.admin import FixStudentAPI
 from exam.views.admin import GetAllStudentAPI
-from exam.views.admin import GetIdStuAllAPI
-from exam.views.admin import GetNameStuAllAPI
+from exam.views.admin import GetAllContentAPI
+# from exam.views.admin import GetIdStuAllAPI
+# from exam.views.admin import GetNameStuAllAPI
 
 urlpatterns = [
-    url(r"get-examstudent/?$", GetExamStudentAPI.as_view(), name="get-examstudent"),
-    url(r"get-idstudent/?$", GetIdStudentAPI.as_view(), name="get-idstudent"),
-    url(r"get-namestudent/?$", GetNameStudentAPI.as_view(), name="get-namestudent"),
+    url(r"get-allcontent/?$", GetAllContentAPI.as_view(), name="get-allcontent"),
+    # url(r"get-idstudent/?$", GetIdStudentAPI.as_view(), name="get-idstudent"),
+    # url(r"get-namestudent/?$", GetNameStudentAPI.as_view(), name="get-namestudent"),
     url(r"add-student/?$", AddStudentAPI.as_view(), name="add-student"),
     url(r"delete-student/?$", DeleteStudentAPI.as_view(), name="delete-student"),
     url(r"fix-student/?$", FixStudentAPI.as_view(), name="fix-student"),
     url(r"get-allstudent/?$", GetAllStudentAPI.as_view(), name="get-allstudent"),
-    url(r"get-idstuall/?$", GetIdStuAllAPI.as_view(), name="get-idstuall"),
-    url(r"get-namestuall/?$", GetNameStuAllAPI.as_view(), name="get-namestuall"),
+    url(r"get-examstudent/?$", GetExamStudentAPI.as_view(), name="get-examstudent"),
+    # url(r"get-idstuall/?$", GetIdStuAllAPI.as_view(), name="get-idstuall"),
+    # url(r"get-namestuall/?$", GetNameStuAllAPI.as_view(), name="get-namestuall"),
 ]
