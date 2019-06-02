915392060a1e636f5fc47f993e3b7ef18d6ec765
hanlinghao <417564526@qq.com>
2019-06-02 18:25:31 +0800

    fix: redundant slash in url (#172)

--- lab/urls/admin.py
+++ lab/urls/admin.py
@@ -5,7 +5,7 @@ from ..views.admin import GetLabsAPI, GetSubmissionFileAPI, GetLabAPI, DeleteLab
 urlpatterns = [
     url(r"get-my-labs/?$", GetLabsAPI.as_view(), name="get-my-labs"),
     path('delete-lab/', DeleteLabAPI.as_view(), name='delete-lab'),
-    path('get-submission-file/', GetSubmissionFileAPI.as_view(), name='get-submission-file'),
+    path('get-submission-file', GetSubmissionFileAPI.as_view(), name='get-submission-file'),
     path('get-lab/', GetLabAPI.as_view(), name='get-lab'),
     path('create-lab', CreateLabAPI.as_view(), name='create-lab'),
     path('edit-lab', EditLabAPI.as_view(), name='edit-lab')
