d620f732f3d247573cae79acdbdf8c4729a05e23
Li Guo <gl_1997@outlook.com>
2019-06-04 20:15:57 +0800

    添加接口

--- course/urls/student.py
+++ course/urls/student.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
-from course.views.student import GetAllCourseAPI,GetAllMessageAPI,GetMessageOfCourseAPI
+from course.views.student import GetAllCourseAPI,GetAllMessageAPI,GetMessageOfCourseAPI,GetMyCourseAPI,GetMyCourseByIDAPI
 
 urlpatterns = [
     url(r'get-all-course/?$', GetAllCourseAPI.as_view(), name="get-all-course"),
     url(r'get-all-message/?$', GetAllMessageAPI.as_view(), name="get-all-message"),
     url(r'get-course-message/?$', GetMessageOfCourseAPI.as_view(), name="get-course-message"),
+    url(r'get-my-course/?$',GetMyCourseAPI.as_view(),name="get-my-course"),
+    url(r'get-my-course-by-id/?$',GetMyCourseByIDAPI.as_view(),name="get-my-course-by-id")
 ]
